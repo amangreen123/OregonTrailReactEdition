225f9229752158d0d9814eb3e92f483d9ba4cfbd
amangreen123 <Aaron.green1@marist.edu>
2023-05-22 23:34:22 -0400

    slight edits to <p> will clean it up when I do more

--- oregontrail/src/mainmenuPage.js
+++ oregontrail/src/mainmenuPage.js
@@ -11,13 +11,16 @@ function MainmenuPage (){
     return(
         <div className="App" style={{ backgroundImage:`url(${bg})`,backgroundRepeat:"no-repeat",height: '1000px',backgroundSize:"cover",backgroundPosition:"center"}}>
             <body>
-            <h1>This is the beginning of amazing journey...</h1>
-            <h1>Of The Oregon Trail</h1>
-            <h1>Choose one of the Options</h1>
-            <p>1. Travel the trail</p>
-            <p>2. Learn about the trail</p>
-            <p>3. See the Oregon top 10</p>
-            <p>4. Turn Sound (Off / On)</p>
+            <ol>
+                <h1>This is the beginning of amazing journey...</h1>
+                <h1>Of The Oregon Trail</h1>
+                <h1>Choose one of the Options</h1>
+                <p>1. Travel the trail</p>
+                <p>2. Learn about the trail</p>
+                <p>3. See the Oregon top 10</p>
+                <p>4. Turn Sound (Off / On)</p>
+            </ol>
+
             </body>
             <input onKeyDown={Navigation}/>
         </div>
