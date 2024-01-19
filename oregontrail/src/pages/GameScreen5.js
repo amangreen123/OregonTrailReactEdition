a20fd0567b0a9afc099f24225ffd802d836590f5
amangreen123 <Aaron.green1@marist.edu>
2024-01-18 19:22:57 -0500

    Displaying group members

--- oregontrail/src/pages/GameScreen5.js
+++ oregontrail/src/pages/GameScreen5.js
@@ -15,10 +15,17 @@ const GameScreen5 = () => {
         <div>
             <p>Here is the information you put in.</p>
             <ul>
-                <li>Leader's Profession:{playerProfession}</li>
-                <li>Leader's Name:{playerName}</li>
-                <li>Party Members:{groupNames}</li>
-                <li>Starting Month:{startMonth}</li>
+                <li>Leader's Profession: {playerProfession}</li>
+                <li>Leader's Name: {playerName}</li>
+                <li>Party Members:
+                <ul>
+                    <li>{groupNames[0]}</li>
+                    <li>{groupNames[1]}</li>
+                    <li>{groupNames[2]}</li>
+                    <li>{groupNames[3]}</li>
+                </ul>
+                </li>
+                <li>Starting Month:  {startMonth}</li>
             </ul>
         </div>
     );
