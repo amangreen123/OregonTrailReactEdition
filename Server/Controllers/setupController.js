a46c8de513db7ad00f02e54fabc3b8137eabd822
amangreen123 <Aaron.green1@marist.edu>
2024-01-28 04:39:43 -0500

    Attempt at dealing with axios put issue

--- Server/Controllers/setupController.js
+++ Server/Controllers/setupController.js
@@ -21,34 +21,67 @@ exports.setupPlayerData = function(req, res) {
     const startMonth = req.body.startMonth;
     const groupmembers = req.body.playerNames;
 
-    const query = {
-        //text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5) ON CONFLICT (playername) DO UPDATE SET playername = $1, playerprofession = $2, playermoney = $3, startmonth = $4, groupmembers = $5',
-        //text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5) RETURNING *',
-        //text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5)',
+    // const query = {
+    //     //text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5) ON CONFLICT (playername) DO UPDATE SET playername = $1, playerprofession = $2, playermoney = $3, startmonth = $4, groupmembers = $5',
+    //     text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5) RETURNING *',
+    //     //text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5)',
+    //     //text: 'UPDATE playerdata SET playername = $1, playerprofession = $2, playermoney = $3, startmonth = $4, groupmembers = $5 WHERE playername = $1 RETURNING *',
+    //     values: [playerName, playerProfession, playerMoney, startMonth, groupmembers]
+    // }
 
-        
+    const existingRowQuery = {
+        text: 'SELECT * FROM playerdata WHERE playername = $1',
+        values: [playerName]
     }
 
-
-    db.query(query, (err, dbRes) => {
+    db.query(existingRowQuery, (err, dbRes) => {
         if (err) {
             console.log(err.stack)
         } else {
-            console.log(dbRes.rows)
-            startGameData.playerNames = [playerName];
-            startGameData.playerNames = startGameData.playerNames.concat(req.body.playerNames);
-            console.log("Player Names: ", startGameData.playerNames);
-            startGameData.playerProfession = req.body.playerProfession;
-            console.log("Player Profession: ", startGameData.playerProfession);
-            startGameData.playerStatus = ["Alive", "Alive", "Alive", "Alive", "Alive"];
-            startGameData.playerMoney = req.body.playerMoney
-            startGameData.startMonth = req.body.startMonth;
+            if(dbRes.rows.length > 0){
+                console.log("Player exists, updating row");
+                const updateQuery = {
+                    text: 'UPDATE playerdata SET playername = $1, playerprofession = $2, playermoney = $3, startmonth = $4, groupmembers = $5 WHERE playername = $1 RETURNING *',
+                    values: [playerName, playerProfession, playerMoney, startMonth, groupmembers]
+                }
+                db.query(updateQuery, (err, dbRes) => {
+                    if (err) {
+                        console.log(err.stack)
+                    } else {
+                        console.log(dbRes.rows)
+                        startGameData.playerNames = [playerName];
+                        startGameData.playerNames = startGameData.playerNames.concat(req.body.playerNames);
+                        console.log("Player Names: ", startGameData.playerNames);
+                        startGameData.playerProfession = req.body.playerProfession;
+                        console.log("Player Profession: ", startGameData.playerProfession);
+                        startGameData.playerStatus = ["Alive", "Alive", "Alive", "Alive", "Alive"];
+                        startGameData.playerMoney = req.body.playerMoney
+                        startGameData.startMonth = req.body.startMonth;
+                    }
+                });
+            } else {
+                console.log("Player does not exist, creating new row");
+                const insertQuery = {
+                    text: 'INSERT INTO playerdata(playername, playerprofession, playermoney, startmonth, groupmembers) VALUES($1, $2, $3, $4, $5) RETURNING *',
+                    values: [playerName, playerProfession, playerMoney, startMonth, groupmembers]
+                }
+                db.query(insertQuery,updateQuery);
+            }
         }
+    });
+
+    function updateQuery(err, dbRes){
+        if (err){
+            console.log(err.stack)
+            dbRes.status(500).send('Error updating player data');
+        }else{
+            console.log(dbRes.rows)
+    }
+
 
         res.setHeader('Content-Type', 'application/json');
         res.send(startGameData);
-
-    });
+}
 }
 
 
