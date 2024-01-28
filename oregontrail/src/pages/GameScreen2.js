3d815a6fc733cd2bf2d67b0a0b9f413693334934
amangreen123 <Aaron.green1@marist.edu>
2024-01-28 15:36:15 -0500

    Refactoring gamescreen 2 with out put request

--- oregontrail/src/pages/GameScreen2.js
+++ oregontrail/src/pages/GameScreen2.js
@@ -1,62 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../components/global.css";
-import {useDispatch} from "react-redux";
-import axios from 'axios';
+import { useDispatch } from "react-redux";
 import bg from "../images/gettyimages-3090888-2.jpg";
 
 function GameScreen2() {
-    const [leaderName, setleaderName] = useState('');
+    const [leaderName, setLeaderName] = useState('');
     const dispatch = useDispatch();
 
     const handleNameChange = (event) => {
-        setleaderName(event.target.value);
+        setLeaderName(event.target.value);
     };
 
-    const updatePlayerName = async () => {
-        try {
-            const response = await axios.put('http://localhost:8000/api/setup/createPlayer', {
-                    playerName: [leaderName],
-            });
+    const updatePlayerName = () => {
+        dispatch({
+            type: "updatePlayerName",
+            payload: { playerName: leaderName },
+        });
+    }
 
-            const updateName = response.data.playerName;
-                setleaderName(updateName);
-
-                dispatch({
-                    type: "updatePlayerName",
-                    payload: { playerName: leaderName},
-                });
-
-                console.log("Player Name :", response.data.playerName)
-                console.log("Status Code:", response.status)
-
-        } catch (error) {
-
-            if(error.response.status === 400){
-                return Promise.reject(error);
-            }
-            console.error('Error updating player name:', error);
+    useEffect(() => {
+        if (leaderName !== "") {
+            updatePlayerName();
         }
+    }, [leaderName, dispatch]);
 
-
-    };
-
-    const buttonClick = (e) => {
+    const buttonClick = () => {
         updatePlayerName();
-        //e.preventDefault();
-        window.location.href = "/GameScreen3"
+        window.location.href = "/GameScreen3";
     }
 
     return (
-        <div className="setup" style={{backgroundImage: `url(${bg})`, backgroundRepeat: "no-repeat", height: "1000px", backgroundSize: "cover", backgroundPosition: "center",}}>
+        <div className="setup" style={{ backgroundImage: `url(${bg})`, backgroundRepeat: "no-repeat", height: "1000px", backgroundSize: "cover", backgroundPosition: "center" }}>
             <h1>What is the name of the leader?</h1>
             <label>
                 Leader Name:
-                <input onChange={(event)=>{
-                    handleNameChange(event);
-                }} type="text" value={leaderName}/>
+                <input onChange={(event) => handleNameChange(event)} type="text" value={leaderName} />
             </label>
-            <button className="button-1" id="page1sub" onClick={buttonClick} >Next Page</button>
-
+            <button className="button-1" id="page1sub" onClick={buttonClick}>Next Page</button>
         </div>
     );
 }
