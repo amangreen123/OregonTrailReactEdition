08bf79695707eff6314e2662fa8deb147753a984
amangreen123 <Aaron.green1@marist.edu>
2024-01-28 15:47:59 -0500

    Refactoring Game Screen 3 and 4

--- oregontrail/src/pages/GameScreen3.js
+++ oregontrail/src/pages/GameScreen3.js
@@ -1,82 +1,85 @@
-import React, { useState } from 'react';
-import axios from "axios";
-import {useDispatch,useSelector} from "react-redux";
-import bg from "../images/gettyimages-3090888-2.jpg";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import bg from '../images/gettyimages-3090888-2.jpg';
 
 const GameScreen3 = () => {
-
     const [groupNames, setGroupNames] = useState({
-        player1: "",
-        player2: "",
-        player3: "",
-        player4: "",
+        player1: '',
+        player2: '',
+        player3: '',
+        player4: '',
     });
 
     const dispatch = useDispatch();
-    console.log("Redux", useSelector((state) => state));
-    const updateGroupName = async () => {
+    const reduxState = useSelector((state) => state);
+    console.log('Redux State:', reduxState);
 
-        try {
-            const response = await axios.put('http://localhost:8000/api/setup/createPlayer', {
-                playerNames: [groupNames.player1, groupNames.player2, groupNames.player3, groupNames.player4],
-            });
-
-            const updatedPlayerNames = response.data.playerNames;
-
-            setGroupNames((prev) => ({
-                ...prev,
-                player1: updatedPlayerNames[0],
-                player2: updatedPlayerNames[1],
-                player3: updatedPlayerNames[2],
-                player4: updatedPlayerNames[3],
-            }));
-
-            dispatch ({
-                type: "updateGroupNames",
-                payload: {playerNames: [groupNames.player1, groupNames.player2, groupNames.player3, groupNames.player4]},
-            });
-
-            console.log("Player Names :", response.data.playerNames)
-            console.log("Repsonse :", response.data)
-            console.log("Group Names :", groupNames.player1)
-            console.log("Response :", response)
-            console.log(response.status)
-
-        } catch (error) {
-            console.error('Error updating player name:', error);
-        }
+    const updateGroupName = () => {
+        dispatch({
+            type: 'updateGroupNames',
+            payload: {
+                playerNames: [
+                    groupNames.player1,
+                    groupNames.player2,
+                    groupNames.player3,
+                    groupNames.player4,
+                ],
+            },
+        });
     };
 
+    useEffect(() => {
+        if (
+            groupNames.player1 !== '' &&
+            groupNames.player2 !== '' &&
+            groupNames.player3 !== '' &&
+            groupNames.player4 !== ''
+        ) {
+            updateGroupName();
+        }
+    }, [groupNames, dispatch]);
+
     const handleGroupNameChange = (e) => {
-        // Destructure the 'name' and 'value' properties from the event target
-        const {name, value} = e.target;
-        // Update the state using the previous state
-        setGroupNames((prev) => {
-            return {
-                ...prev, // Spread the previous state to keep existing values
-                [name]: value, // Update the specific property with the new value
-            };
-        })
-        console.log("name:", name);
-        console.log("value:", value);
-    }
+        const { name, value } = e.target;
+        setGroupNames((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
 
     const buttonClick = () => {
+        if (
+            groupNames.player1 === '' ||
+            groupNames.player2 === '' ||
+            groupNames.player3 === '' ||
+            groupNames.player4 === ''
+        ) {
+            alert('Please enter a name for each player');
+            return;
+        }
         updateGroupName();
-        window.location.href = "/GameScreen4"
-    }
+        window.location.href = '/GameScreen4';
+    };
 
     return (
-        <div className="setup" style={{backgroundImage: `url(${bg})`, backgroundRepeat: "no-repeat", height: "1000px", backgroundSize: "cover", backgroundPosition: "center",}}>
+        <div
+            className="setup"
+            style={{
+                backgroundImage: `url(${bg})`,
+                backgroundRepeat: 'no-repeat',
+                height: '1000px',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
             <p>What are the names of the Wagon Party</p>
-            Player Name: <input name="player1" type="text" onChange={handleGroupNameChange}/><br/>
-            Player Name: <input name="player2" type="text" onChange={handleGroupNameChange}/><br/>
-            Player Name: <input name="player3" type="text" onChange={handleGroupNameChange}/><br/>
-            Player Name: <input name="player4" type="text" onChange={handleGroupNameChange}/><br/>
-            <input type="button" class="button-1" id="page2sub" value="Next" onClick={buttonClick}/>
+            Player Name: <input name="player1" type="text" onChange={handleGroupNameChange} /><br />
+            Player Name: <input name="player2" type="text" onChange={handleGroupNameChange} /><br />
+            Player Name: <input name="player3" type="text" onChange={handleGroupNameChange} /><br />
+            Player Name: <input name="player4" type="text" onChange={handleGroupNameChange} /><br />
+            <input type="button" className="button-1" id="page2sub" value="Next" onClick={buttonClick} />
         </div>
     );
 };
-
 
 export default GameScreen3;
--- oregontrail/src/pages/GameScreen4.js
+++ oregontrail/src/pages/GameScreen4.js
@@ -1,62 +1,68 @@
-import React, { useState } from 'react';
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import bg from "../images/gettyimages-3090888-2.jpg";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import bg from '../images/gettyimages-3090888-2.jpg';
 
 const GameScreen4 = () => {
-    const [startMonth, setStartMonth] = useState("");
+    const [startMonth, setStartMonth] = useState('');
     const dispatch = useDispatch();
+    const reduxState = useSelector((state) => state);
 
-    const updateMonth = async (event) => {
-        let month = event.target.id; // Use event.target.id to get the selected month
-        try {
-            const response = await axios.put('http://localhost:8000/api/setup/createPlayer', {
-                startMonth: month,
-            });
+    const updateMonth = (event) => {
+        const month = event.target.id;
 
-            console.log("Response:", response);
-            console.log("Response Data:", response.data);
-            console.log("Response Data Start Month:", response.data.startMonth);
+        dispatch({
+            type: 'updateStartMonth',
+            payload: { startMonth: month },
+        });
+    };
 
-            if (response && response.data) {
-                setStartMonth(response.data.startMonth);
-
-                dispatch({
-                    type: "updateStartMonth",
-                    payload: { startMonth: response.data.startMonth },
-                });
-
-                console.log("Start Month:", response.data.startMonth);
-            } else {
-                console.error("Error updating Month");
-            }
-        } catch (error) {
-            console.error('Error updating Month', error);
+    useEffect(() => {
+        if (startMonth !== '') {
+            updateMonth();
         }
-
-    }
+    }, [startMonth, dispatch]);
 
     const buttonClick = () => {
-        if (startMonth === "") {
-            alert("Please select a month")
+        if (startMonth === '') {
+            alert('Please select a month');
             return;
         }
-        updateMonth()
-        window.location.href = "/GameScreen5"
-    }
+        updateMonth();
+        window.location.href = '/GameScreen5';
+    };
 
     return (
-        <div className="setup" style={{backgroundImage: `url(${bg})`, backgroundRepeat: "no-repeat", height: "1000px", backgroundSize: "cover", backgroundPosition: "center",}}>
+        <div
+            className="setup"
+            style={{
+                backgroundImage: `url(${bg})`,
+                backgroundRepeat: 'no-repeat',
+                height: '1000px',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
             <p>Which month would you like to leave</p>
             <ol id="setupQuestions2">
-                <li id="March" onClick={updateMonth}>March</li>
-                <li id="April" onClick={updateMonth}>April</li>
-                <li id="May" onClick={updateMonth}>May</li>
-                <li id="June" onClick={updateMonth}>June</li>
-                <li id="July" onClick={updateMonth}>July</li>
+                <li id="March" onClick={updateMonth}>
+                    March
+                </li>
+                <li id="April" onClick={updateMonth}>
+                    April
+                </li>
+                <li id="May" onClick={updateMonth}>
+                    May
+                </li>
+                <li id="June" onClick={updateMonth}>
+                    June
+                </li>
+                <li id="July" onClick={updateMonth}>
+                    July
+                </li>
             </ol>
-            <button className="button-1" id="page1sub" onClick={buttonClick} >Next Page</button>
-
+            <button className="button-1" id="page1sub" onClick={buttonClick}>
+                Next Page
+            </button>
         </div>
     );
 };
