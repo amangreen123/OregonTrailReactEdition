b204ecc21d70f7d1ddb6a24f0d675a6a08f06249
amangreen123 <Aaron.green1@marist.edu>
2024-07-24 12:18:04 -0400

    differences menu

--- oregontrail/src/pages/GameScreen1.js
+++ oregontrail/src/pages/GameScreen1.js
@@ -51,6 +51,12 @@ const GameScreen1 = () => {
         updatePlayerData();
     }
 
+    const differencesMenuItem = () => {
+        alert("Banker: Starts with $2000, Carpenter: Starts with $1500, Farmer: Starts with $1000")
+        return;
+
+    }
+
     return (
         <div >
             <p>Choose your Profession.</p>
@@ -59,7 +65,7 @@ const GameScreen1 = () => {
                 <li id="bankerMenuItem" onClick={() => handleProfession("Banker")} onChange={playerSubmit}>Be a banker from Boston </li>
                 <li id="carpenterMenuItem" onClick={() => handleProfession("Carpenter")} onChange={playerSubmit}>Be a carpenter from Ohio</li>
                 <li id="farmerMenuItem" onClick={() => handleProfession("Farmer")} onChange={playerSubmit}>Be a farmer from Illinois</li>
-                <li id="differencesMenuItem">Find out the differences between the choices</li>
+                <li id="differencesMenuItem" onClick={() => differencesMenuItem()}>Find out the differences between the choices</li>
             </ol>
             <div>You Have Chosen {playerProfession} You have this Amount of Money {playerMoney}</div>
             <button onClick={event => window.location.href="/GameScreen2"}> Next Page </button>
